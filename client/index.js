1c5a606d6fdf9a1e26ff3be54bb87d021ac16a6d
bunchito <enotragalaxiahay@outlook.com>
2018-05-03 12:49:46 -0700

    Changes

--- client/index.js
+++ client/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import SongList from './components/SongList';
+
+// This assumes that GraphQL server is available in /graphql
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Route path="/" component={SongList} />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector('#root'));
